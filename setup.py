66f09b232a439ff48295686a513ca5bcf6bf002a
Yngve Mardal Moe <yngve.m.moe@gmail.com>
2019-09-03 14:30:12 +0200

    Updated setup files

--- setup.py
+++ setup.py
@@ -1,12 +1,3 @@
 from setuptools import setup, find_packages
 
-setup(
-    name="SURNAME_FIRSTNAME_exersices",  # No capital letters! e.g. "moe_yngve_exersices"
-    version="0.0.1",
-    author="FULL NAME",  # e.g. "Yngve Mardal Moe"
-    author_email="your@email.no",
-    description="Exersices for INF200",
-    packages=find_packages("src"),
-    package_dir={"": "src"},
-    include_package_data=True,
-)
+setup()
